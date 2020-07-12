522d99cd5aa0b91f18b64306764b17365bbf5620
yxxhero <you@example.com>
2020-07-12 17:55:02 +0800

    update

---
+++ scripts/update_index.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: update_index.sh
+#
+#         USAGE: ./update_index.sh
+#
+#   DESCRIPTION: 
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 07/12/2020 03:56:28 PM
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                                  # Treat unset variables as an error
+cd ../content
+hugo-algolia -s --config ../config.yaml -i "zh/**"  --output ../public/algolia.json
